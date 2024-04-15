3ac4968c9fbf414fa513e8f8584be4b8e25d2be1
Darby Oleary <darby.oleary@live.com>
2024-04-15 00:18:56 -0500

    Began work

--- main.go
+++ main.go
@@ -1,8 +1,28 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/veandco/go-sdl2/sdl"
 )
+
+//checks if two rectangles collide with eachother
+func checkBounds(r1 sdl.Rect, r2 sdl.Rect) bool{
+	r1End := r1.X + r1.W
+	width := r2.X + r2.W
+
+	if  (r1.X >= r2.X && r1.X <= width) || (r1End >= r2.X && r1End <= width){
+		r1Height := r1.Y + r1.H
+		height := r2.Y + r2.H
+		if (r1.Y >= r2.Y && r1.Y <= height) || (r1Height >= r2.Y && r1Height <= height){
+			return true
+		}
+	}
+
+	return false
+}
+
+
 
 func main() {
 	sdl.Init(sdl.INIT_EVERYTHING)
@@ -13,13 +33,32 @@ func main() {
 	if err != nil {
 		return
 	}
+	defer renderer.Destroy()
 
 	window.Show()
 
 	renderer.Clear()
+	sprites, err := sdl.LoadBMP("sprites.bmp")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer sprites.Free()
+
+	tex, err := renderer.CreateTextureFromSurface(sprites)
+	if err != nil {
+		return
+	}
+	defer tex.Destroy()
+
+	//sets up rectangles to get texture and set location of sprites
+	spriteRect := sdl.Rect{X: 0, Y: 0, W: 16, H: 16}
+
+	destRect := sdl.Rect{X: 0, Y: 0, W: 32, H: 32}
 
 	for !closeRequested {
 		renderer.Clear()
+		renderer.Copy(tex, &spriteRect, &destRect)
 		renderer.Present()
 
 		//make sure to put PollEvent to a variable because the rendering thread can go to nil mid-check and cause a null reference error
@@ -27,5 +66,6 @@ func main() {
 		if event != nil && event.GetType() == sdl.QUIT {
 			closeRequested = true
 		}
+
 	}
 }

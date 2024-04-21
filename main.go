df207715110172eb61739152b3df4c4988ce5b9b
Darby Oleary <darby.oleary@live.com>
2024-04-21 04:20:35 -0500

    Added movement for player, barrel, and early ladder generation

--- main.go
+++ main.go
@@ -2,19 +2,30 @@ package main
 
 import (
 	"fmt"
+	"math"
+	"math/rand"
 	"time"
 
 	"github.com/veandco/go-sdl2/sdl"
 )
 
 const (
-	screenW = 640
-	screenH = 480
+	screenW     = 640
+	screenH     = 960
+	perJump     = 7
+	perVertex   = 15
+	speed       = 2
+	jumpSpeed   = 5
+	barrelSpeed = 2
 )
 
 var objects = make(map[sdl.Rect][]*Sprite)
 
 var boundaries = make(map[sdl.Rect][]*Boundary)
+
+var tiles []*sdl.Rect
+
+var ladders []*sdl.Rect
 
 // checks if two rectangles collide with eachother
 func checkBounds(r1 sdl.Rect, r2 sdl.Rect) bool {
@@ -36,15 +47,7 @@ func checkBounds(r1 sdl.Rect, r2 sdl.Rect) bool {
 func getTile(x int, y int) sdl.Rect {
 	xOffset := int32(x / 32)
 
-	if x%32 > 0 {
-		xOffset++
-	}
-
 	yOffset := int32(y / 32)
-
-	if y%32 > 0 {
-		yOffset++
-	}
 
 	return sdl.Rect{X: xOffset * 32, Y: yOffset * 32, W: 32, H: 32}
 }
@@ -76,6 +79,42 @@ func getNearbyTiles(x int, y int) []*sdl.Rect {
 	return tiles
 }
 
+func getFloorHeight(x int, y int) int {
+	player := getTile(x, y)
+
+	//check if it's at the very bottom and set to a default
+	if player.Y >= screenH {
+		return screenH - 32
+	}
+
+	for player.Y < screenH {
+		floors, in := boundaries[player]
+		if !in {
+			player.Y += 32
+			continue
+		}
+
+		groundH := floors[0].Y
+
+		for _, floor := range floors {
+			if math.Abs(float64(y-floor.Y)) < float64(groundH) {
+				groundH = floor.Y
+			}
+		}
+		return groundH
+	}
+
+	return screenH - 32
+}
+
+// checks if there is a floor tile in that tile
+func hasFloorTile(x int, y int) bool {
+	tile := getTile(x, y)
+	t, in := boundaries[tile]
+	fmt.Println(t)
+	return in
+}
+
 // given a coordinate pair and a screen tile, check if any of the boundaries in that tile collide with the given point
 func checkCollide(x int, y int, rect *sdl.Rect) bool {
 	bound, exists := boundaries[*rect]
@@ -93,6 +132,52 @@ func checkCollide(x int, y int, rect *sdl.Rect) bool {
 	}
 
 	return false
+}
+
+// creates girders with random varying heights from eachother on the screen
+func createGirders() {
+	for i := 32; i <= screenH-32; i += 128 {
+		height := int32(i)
+		ladderCount := 0
+		for j := 0; j <= 640; j += 32 {
+			//randomly stays the same as the last beam tile, or gets higher/lower
+			switch rand.Intn(3) {
+			case 1:
+				height++
+			case 2:
+				height--
+			}
+			//testing to make sure (highly unlikely) the boundary doesn't entirely leave the tile it should begin in
+			if math.Abs(float64(i)-float64(height)) == 32 {
+				height = int32(i)
+			}
+			//because the boundary tiles vary in y position, they will often go across multiple screen tiles, so they need to be registered to more than one tile
+			block := sdl.Rect{X: int32(j), Y: height, W: 32, H: 32}
+			//screenLoc is the tile where the origin (top left) of the boundary is, so the boundary is either perfectly in that tile or partially in it and the one below it
+			screenLoc := getTile(j, int(height))
+
+			if screenLoc.Y == int32(i) {
+				boundaries[block] = append(boundaries[block], &Boundary{X: j, Y: int(height), W: 32, H: 32})
+			} else {
+				boundaries[screenLoc] = append(boundaries[screenLoc], &Boundary{X: j, Y: int(height), W: 32, H: 32})
+				//also add the tile underneath
+				under := sdl.Rect{X: screenLoc.X, Y: screenLoc.Y + 32, H: 32, W: 32}
+				boundaries[under] = append(boundaries[under], &Boundary{X: j, Y: int(height), W: 32, H: 32})
+			}
+
+			if i > 32 && j == 640 {
+				ladderCount++
+				height -= 32
+				distanceToBeam := 128
+				for distanceToBeam != 0 {
+					ladders = append(ladders, &sdl.Rect{X: int32(j) - 32, Y: height, H: 32, W: 32})
+					height -= 32
+					distanceToBeam -= 32
+				}
+			}
+			tiles = append(tiles, &block)
+		}
+	}
 }
 
 func main() {
@@ -122,22 +207,49 @@ func main() {
 	}
 	defer tex.Destroy()
 
-	//sets up rectangles to get texture and set location of sprites
-	spriteRect := sdl.Rect{X: 0, Y: 0, W: 16, H: 16}
+	down, left, right := false, false, false
 
-	destRect := sdl.Rect{X: 0, Y: 0, W: 32, H: 32}
+	jump := false
 
-	up := false
+	//controls how many frames a jump is
+	jumpFrames := perJump
+	//how many frames to hold at the maximum height of the jump before it begins to descend
+	framesAtVertex := perVertex
 
-	test := createSprite(renderer, tex, &sdl.Rect{X: 75, Y: 75, W: 32, H: 32}, 3, 5, "player")
+	//this stores the height from where the player jumped, when returning to the ground it's stored so if he jumps into an above tile he doesn't teleport to it thinking that's the ground
+	landHeight := 0
 
-	test.addFrame(sdl.Rect{X: 0, Y: 0, W: 16, H: 16})
-	test.addFrame(sdl.Rect{X: 16, Y: 0, W: 16, H: 16})
-	test.addFrame(sdl.Rect{X: 32, Y: 0, W: 16, H: 16})
+	//creates the player sprite
+	player := createSprite(renderer, tex, &sdl.Rect{X: 75, Y: 900, W: 32, H: 32}, 3, 5, "player")
+
+	//adds frames and different animations to the player sprite
+	//walking
+	player.addFrame(sdl.Rect{X: 0, Y: 0, W: 16, H: 16}, 0)
+	player.addFrame(sdl.Rect{X: 16, Y: 0, W: 16, H: 16}, 0)
+	player.addFrame(sdl.Rect{X: 32, Y: 0, W: 16, H: 16}, 0)
+	//jumping
+	player.addAnimationSet(1, 1)
+	player.addFrame(sdl.Rect{X: 0, Y: 16, W: 16, H: 16}, 1)
+	//climbing
+	player.addAnimationSet(2, 5)
+	player.addFrame(sdl.Rect{X: 16, Y: 16, W: 16, H: 16}, 2)
+	player.addFrame(sdl.Rect{X: 32, Y: 16, W: 16, H: 16}, 2)
+
+	//holds how to flip the player
+	flip := sdl.FLIP_NONE
+
+	barrel := createSprite(renderer, tex, &sdl.Rect{X: 600, Y: 900, W: 32, H: 32}, 3, 5, "barrel")
+	barrel.addFrame(sdl.Rect{X: 48, Y: 0, W: 16, H: 16}, 0)
+	barrel.addFrame(sdl.Rect{X: 64, Y: 0, W: 16, H: 16}, 0)
+	barrel.addFrame(sdl.Rect{X: 80, Y: 0, W: 16, H: 16}, 0)
+	barrel.addFrame(sdl.Rect{X: 96, Y: 0, W: 16, H: 16}, 0)
+
+	createGirders()
 
 	for !closeRequested {
 		//event checking for user input
 		for event := sdl.PollEvent(); event != nil; event = sdl.PollEvent() {
+			//can ignore warning because it's uneeded as a variable
 			switch event.(type) {
 			case *sdl.QuitEvent:
 				closeRequested = true
@@ -146,12 +258,31 @@ func main() {
 				if keyEvent.Type == sdl.KEYDOWN {
 					switch keyEvent.Keysym.Scancode {
 					case sdl.SCANCODE_W, sdl.SCANCODE_UP:
-						up = true
+						down = false
+						if !jump {
+							jump = true
+							landHeight = int(player.position.Y)
+						}
+					case sdl.SCANCODE_S, sdl.SCANCODE_DOWN:
+						down = true
+					case sdl.SCANCODE_A, sdl.SCANCODE_LEFT:
+						left = true
+						right = false
+						flip = sdl.FLIP_NONE
+					case sdl.SCANCODE_D, sdl.SCANCODE_RIGHT:
+						left = false
+						right = true
+						flip = sdl.FLIP_HORIZONTAL
 					}
 				} else if keyEvent.Type == sdl.KEYUP {
 					switch keyEvent.Keysym.Scancode {
+					case sdl.SCANCODE_W, sdl.SCANCODE_UP:
 					case sdl.SCANCODE_S, sdl.SCANCODE_DOWN:
-						up = false
+						down = false
+					case sdl.SCANCODE_A, sdl.SCANCODE_LEFT:
+						left = false
+					case sdl.SCANCODE_D, sdl.SCANCODE_RIGHT:
+						right = false
 					}
 				}
 
@@ -159,13 +290,58 @@ func main() {
 		}
 
 		renderer.Clear()
-		if up {
-			destRect.X += 1
+
+		if jump {
+			if jumpFrames > 0 {
+				player.position.Y -= jumpSpeed
+				jumpFrames--
+			} else {
+				//range of the landing values MUST be greater than the height change per frame or it's possible to slip through
+				if player.position.Y >= int32(getFloorHeight(int(player.position.X), int(player.position.Y))-32) && math.Abs(float64(player.position.Y-int32(landHeight))) <= 5 {
+					jumpFrames = perJump
+					framesAtVertex = perVertex
+					jump = false
+					player.position.Y = int32(getFloorHeight(int(player.position.X), int(player.position.Y)) - 32)
+				} else if framesAtVertex >= 0 {
+					framesAtVertex--
+				} else {
+					player.position.Y += jumpSpeed
+				}
+			}
 		}
-		renderer.Copy(tex, &spriteRect, &destRect)
-		renderer.Copy(tex, &sdl.Rect{X: 64, Y: 0, W: 16, H: 16}, &sdl.Rect{X: 50, Y: 0, W: 32, H: 32})
-		test.animateWithFreezeFrame(!up)
+		if down {
+			player.position.Y += 1
+		}
+		if left {
+			player.position.X -= speed
+		}
+		if right {
+			player.position.X += speed
+		}
+
+		//draws ladders to screen
+		for _, rect := range ladders {
+			renderer.Copy(tex, &sdl.Rect{X: 128, Y: 0, W: 16, H: 16}, rect)
+		}
+
+		//draws girder tiles to screen
+		for _, rect := range tiles {
+			renderer.Copy(tex, &sdl.Rect{X: 112, Y: 0, W: 16, H: 16}, rect)
+		}
+
+		if !jump {
+			player.position.Y = int32(getFloorHeight(int(player.position.X), int(player.position.Y)) - 32)
+			player.animateWithFreezeFrame(0, !(left || right), flip)
+		} else {
+			player.animate(1, flip)
+		}
+
+		barrel.position.X -= barrelSpeed
+		barrel.position.Y = int32(getFloorHeight(int(barrel.position.X), int(barrel.position.Y)) - 32)
+		barrel.animateWithFreezeFrame(0, false, sdl.FLIP_NONE)
+
 		renderer.Present()
+		//rests so game runs at constant 60 fps
 		time.Sleep(time.Second / 60)
 	}
 }
--- sprite.go
+++ sprite.go
@@ -6,11 +6,11 @@ import (
 
 type Sprite struct {
 	//holds pointers to the animation frame locations in spritesheet
-	frames        []sdl.Rect
-	frameCount    int
-	currentFrame  int
-	tick          int
-	ticksPerFrame int
+	frames        [][]sdl.Rect
+	frameCount    []int
+	currentFrame  []int
+	tick          []int
+	ticksPerFrame []int
 	renderer      *sdl.Renderer
 	texture       *sdl.Texture
 	position      *sdl.Rect
@@ -19,53 +19,68 @@ type Sprite struct {
 
 func createSprite(rend *sdl.Renderer, tex *sdl.Texture, pos *sdl.Rect, frames int, ticks int, spriteType string) Sprite {
 	s := Sprite{}
-	s.frameCount = frames
-	s.currentFrame = 0
+	//begin with storage for one animation
+	s.frames = make([][]sdl.Rect, 1)
+	s.frameCount = make([]int, 1)
+	s.ticksPerFrame = make([]int, 1)
+	s.tick = make([]int, 1)
+	s.currentFrame = make([]int, 1)
+	s.frameCount[0] = frames
+	s.currentFrame[0] = 0
 	s.renderer = rend
 	s.texture = tex
 	s.position = pos
-	s.tick = 0
-	s.ticksPerFrame = ticks
+	s.tick[0] = 0
+	s.ticksPerFrame[0] = ticks
 	s.spriteType = spriteType
 
 	return s
 }
 
-func (s *Sprite) addFrame(rect sdl.Rect) {
-	s.frames = append(s.frames, rect)
+// creates room for a new animation for this sprite, takes in the framecount
+func (s *Sprite) addAnimationSet(frames int, ticks int) {
+	s.frames = append(s.frames, make([]sdl.Rect, 0))
+	s.frameCount = append(s.frameCount, frames)
+	s.ticksPerFrame = append(s.ticksPerFrame, ticks)
+	s.tick = append(s.tick, 0)
+	s.currentFrame = append(s.currentFrame, 0)
 }
 
-func (s *Sprite) animate() {
-	if s.tick < s.ticksPerFrame {
-		s.tick++
+func (s *Sprite) addFrame(rect sdl.Rect, set int) {
+	s.frames[set] = append(s.frames[set], rect)
+}
+
+func (s *Sprite) animate(set int, dir sdl.RendererFlip) {
+	if s.tick[set] < s.ticksPerFrame[set] {
+		s.tick[set]++
 	} else {
-		s.tick = 0
-		s.currentFrame++
+		s.tick[set] = 0
+		s.currentFrame[set]++
 	}
 
-	if s.currentFrame == s.frameCount {
-		s.currentFrame = 0
+	if s.currentFrame[set] == s.frameCount[set] {
+		s.currentFrame[set] = 0
 	}
 
-	s.renderer.Copy(s.texture, &s.frames[s.currentFrame], s.position)
+	s.renderer.CopyEx(s.texture, &s.frames[set][s.currentFrame[set]], s.position, 0, nil, dir)
 }
 
-func (s *Sprite) animateWithFreezeFrame(freeze bool) {
+func (s *Sprite) animateWithFreezeFrame(set int, freeze bool, dir sdl.RendererFlip) {
 	if freeze {
-		s.renderer.Copy(s.texture, &s.frames[0], s.position)
+		s.renderer.CopyEx(s.texture, &s.frames[set][0], s.position, 0, nil, dir)
 		return
 	}
 
-	if s.tick < s.ticksPerFrame {
-		s.tick++
+	if s.tick[set] < s.ticksPerFrame[set] {
+		s.tick[set]++
 	} else {
-		s.tick = 0
-		s.currentFrame++
+		s.tick[set] = 0
+		s.currentFrame[set]++
 	}
 
-	if s.currentFrame == s.frameCount {
-		s.currentFrame = 0
+	if s.currentFrame[set] == s.frameCount[set] {
+		s.currentFrame[set] = 0
 	}
 
-	s.renderer.Copy(s.texture, &s.frames[s.currentFrame], s.position)
+	s.renderer.CopyEx(s.texture, &s.frames[set][s.currentFrame[set]], s.position, 0, nil, dir)
 }
